0cf75e6bff8025ce4149e77d589fec9869200139
Jakub Piwko <j.piwko@student.mini.pw.edu.pl>
2022-04-02 02:55:28 +0200

    Update documentation + some changes

--- KM2/functions.R
+++ KM2/functions.R
@@ -1,22 +1,47 @@
 library(lightgbm)
 
+#' LightGBM_function - Train LightGBM model
+#'
+#' @param data  data frame object, containing columns of any kind,
+#' including variable to predict.
+#' @param target  target variable column name
+#' @param ...  additional model hyperparameters in format (arg1 = val1,
+#'  arg2 = val2) etc.
+#'  For list of possible parameters
+#'  visit: https://lightgbm.readthedocs.io/en/latest/Parameters.html
+#'
+#' @return LightGBM model trained on data given as a parameter.
+#' @export
+#'
+#' @examples
+#' data(iris)
+#' split1 <- sample(c(rep(0, 0.7 * nrow(iris)), rep(1, 0.3 * nrow(iris))))
+#' train <- iris[split1 == 0, ]
+#' model <- lightGBM_function(train, "Species",
+#'                              objective = 'multiclass',
+#'                               num_class = 3)
 
-lightGBM_function <- function(data, target, 
-                              num_leaves = 4L,
-                              learning_rate = 1.0,
-                              objective = "multiclass", 
-                              nthread = 2L
-){
+
+lightGBM_function <- function(data, target, ...){
+  
+  params <- list(...)
+  
+  # Tutaj zabiezpeczenie przed niepodaniem argumenty objective
+  # Jeśli go nie będzie, to domyślnie model klasyfikacji binarnej
+  if (is.null(params$objective)) {
+    params <- append(params, list(objective = 'binary'))
+  }
   
   
-  #Gdy nie regresja to trzeba zamienić aby było od 0
-  if (objective != "regression") {
-    data[, target] <- as.numeric(as.factor(data[, target])) - 1L
+  #Gdy nie regresja to trzeba zamienić aby klasy były numeryczne i od 0
+  if (!is.null(params$objective) && params$objective != "regression") {
+    data[ ,target] <- as.numeric(as.factor(data[ ,target])) - 1L
   }
   
   
   
-  #Gdy jest ramka danych, przerabiamy ją na coś pasującego do lightGBM
+  #W sumie faktycznie można założyć, że tylko ramki danych przyjmujemy,
+  #bo za dużo zabawy.
   if ("data.frame" %in% class(data)) {
     
     #Wyciągnięcie targetu i zamiana na macierz ramek
@@ -26,37 +51,32 @@ lightGBM_function <- function(data, target,
     
     #Tworzenie lgb.datasets treningoego
     data <- lgb.Dataset(
-      data = data
-      , label = target_val
-      , free_raw_data = FALSE
+      data = data,
+      label = target_val,
+      free_raw_data = FALSE
     )
   }
   
-  
-  
-  if (objective == "multiclass") {
-    params <- list(
-      objective = "multiclass"
-      , metric = "multi_error"
-      , num_class = length(unique(target_val))
-      , min_data = 1L
-    )
-  }
-  else{
-    params <- list(
-      num_leaves = num_leaves
-      , learning_rate = learning_rate
-      , objective = objective
-      , nthread = nthread
-    )
-  }
-  
-  
-  
+  # if (objective == "multiclass") {
+  #   params <- list(
+  #     objective = "multiclass"
+  #     , metric = "multi_error"
+  #     , num_class = length(unique(target_val))
+  #     , min_data = 1L
+  #   )
+  # }
+  # else{
+  #   params <- list(
+  #     num_leaves = num_leaves
+  #     , learning_rate = learning_rate
+  #     , objective = objective
+  #     , nthread = nthread
+  #   )
+  # }
   
   # Model
   model <- lgb.train(
-    params   = params
+    params = params
     , data = data
     , nrounds =  100L
   )
@@ -65,12 +85,38 @@ lightGBM_function <- function(data, target,
 }
 
 
-lightGBM_predict <- function(model, data){
+##############################################
+
+#' LightGBM_predict - predict target for samples in data.
+#'
+#' @param model Trained LightGBM model
+#' @param data Data frame to perform prediction on
+#'
+#' @return vector od predicted values
+#' @export
+#'
+#' @examples
+#' data(iris)
+#' split1 <- sample(c(rep(0, 0.7 * nrow(iris)), rep(1, 0.3 * nrow(iris))))
+#' train <- iris[split1 == 0, ]
+#' model <- lightGBM_function(train, "Species",
+#'                              objective = 'multiclass',
+#'                               num_class = 3)
+#' test <- iris[split1== 1, ]
+#' test <- test[,1:4]
+#' pred <- lightGBM_predict(model, test)
+
+LightGBM_predict <- function(model, data){
   
   test <- as.matrix(data)
   
   return(predict(model, test))
 }
+
+#################################################
+
+
+
 
 
 ## Przykład użycia
@@ -83,10 +129,5 @@ model <- lightGBM_function(train, "Species" )
 
 
 test <- iris[split1== 1, ]
-test2 <- test[,1:5]
 test <- test[,1:4]
 pred <- lightGBM_predict(model, test)
-
-
-
-
