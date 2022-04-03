d3fbda5f8354cf1b173e7118213a09a2bd8e53a0
niegrzybkowski <56119793+niegrzybkowski@users.noreply.github.com>
2022-04-03 19:25:41 +0200

    Rework training/prediction functions

--- KM2/functions.R
+++ KM2/functions.R
@@ -1,84 +1,93 @@
 library(lightgbm)
 
-#' LightGBM_function - Train LightGBM model
-#'
-#' @param data  data frame object, containing columns of any kind,
-#' including variable to predict.
-#' @param target  target variable column name
-#' @param ...  additional model hyperparameters in format (arg1 = val1,
-#'  arg2 = val2) etc.
+#' LightGBM_function - Train LightGBM model from a dataframe, with some automation.
+#' 
+#' @param data Data frame to perform prediction on. 
+#' All columns must be <numeric>, <factor> (or <character> if `autofactor` parameter is set to `TRUE`).
+#' @param target Target variable column name.
+#' @param objective One of "regression", "binary", "multiclass".
+#' @param autofactor Logical flag to control whether to encode <character> columns of `data` into <factor>. Default is `FALSE`. 
+#' The encoding is likely to be dependent on order, which is not desirable.
+#' @param ...  Additional model parameters to be passed to `lgb.train()`.
+#'  Note that some parameters are set automatically by this wrapper function. These include
+#'  * `...$objective` -> Controlled by `objective`
+#'  * `...$num_class` -> Inferred from `data`.
+#'  * `...$categorical_feature` -> Inferred from `data`.
 #'  For list of possible parameters
 #'  visit: https://lightgbm.readthedocs.io/en/latest/Parameters.html
 #'
-#' @return LightGBM model trained on data given as a parameter.
+#' @return LightGBM model
 #' @export
 #'
 #' @examples
+#' # More examples present in `lgbm_test_*()` suite of functions
 #' data(iris)
 #' split1 <- sample(c(rep(0, 0.7 * nrow(iris)), rep(1, 0.3 * nrow(iris))))
-#' train <- iris[split1 == 0, ]
-#' model <- lightGBM_function(train, "Species",
-#'                              objective = 'multiclass',
-#'                               num_class = 3)
+#' train <- iris[split1 == 0, ]  
+#' model <- lightGBM_function(
+#'   train,
+#'   "Sepal.Length",
+#'   "regression"
+#' )
 
-
-lightGBM_function <- function(data, target, ...){
+lightGBM_function <- function(
+  data,
+  target,
+  objective,
+  ...,
+  autofactor=FALSE,
+  forceconvert=FALSE
+  ){
   
   params <- list(...)
+  stopifnot(
+    # Support only <data.frome>
+    "data.frame" %in% class(data),
+    # Support only simple objectives
+    (objective %in% c("regression", "binary", "multiclass")),
+    is.null(params$objective),
+    # Filled in by this function
+    is.null(params$num_class),
+    is.null(params$categorical_feature)
+  )
   
-  # Tutaj zabiezpeczenie przed niepodaniem argumenty objective
-  # Jeśli go nie będzie, to domyślnie model klasyfikacji binarnej
-  if (is.null(params$objective)) {
-    params <- append(params, list(objective = 'binary'))
+  # Convert all <character> columns to <factor> if requested.
+  # The encoding is likely to be dependent on order, which is not desirable.
+  if (autofactor) {
+    data <- lgbm_character_to_factor(data)
+  } else {
+    lgbm_assert_no_characters(data)
   }
   
+  # Convert all <factor> columns to <numeric>.
+  categorical_columns <- sapply(data, is.factor)
+  data <- lgbm_factor_to_numeric(data)
   
-  #Gdy nie regresja to trzeba zamienić aby klasy były numeryczne i od 0
-  if (!is.null(params$objective) && params$objective != "regression") {
-    data[ ,target] <- as.numeric(as.factor(data[ ,target])) - 1L
+  # Force conversion to <numeric> if requested
+  if (forceconvert) {
+    data <- data.frame(lapply(data, as.numeric))
+  } else {
+    lgbm_assert_all_numeric(data)
+  }
+  # Fill in parameters based on 
+  if (objective == "regression") {
+    params$objective = "regression"
+  } else if (objective == "multiclass") {
+    params$objective = "multiclass"
+    params$num_classes = length(unique(data[, target]))
+    # LGB expects a numeric vector starting from 0, R encodes factors from 1
+    data[, target] <- data[, target] - 1L 
+  } else {
+    params$objective = "binary"
   }
   
-  
-  
-  #W sumie faktycznie można założyć, że tylko ramki danych przyjmujemy,
-  #bo za dużo zabawy.
-  if ("data.frame" %in% class(data)) {
-    
-    #Wyciągnięcie targetu i zamiana na macierz ramek
-    target_val <- data[, target]
-    data <- data[ , !(names(data) %in% target)]
-    data <- as.matrix(data[, 1:ncol(data)])
-    
-    #Tworzenie lgb.datasets treningoego
-    data <- lgb.Dataset(
-      data = data,
-      label = target_val,
-      free_raw_data = FALSE
-    )
-  }
-  
-  # if (objective == "multiclass") {
-  #   params <- list(
-  #     objective = "multiclass"
-  #     , metric = "multi_error"
-  #     , num_class = length(unique(target_val))
-  #     , min_data = 1L
-  #   )
-  # }
-  # else{
-  #   params <- list(
-  #     num_leaves = num_leaves
-  #     , learning_rate = learning_rate
-  #     , objective = objective
-  #     , nthread = nthread
-  #   )
-  # }
-  
-  # Model
+  # Convert dataframe to lgb.dataset
+  dataset <- lgbm_df_to_train(data, target, categorical_columns)
+
+  # Train model
   model <- lgb.train(
-    params = params
-    , data = data
-    , nrounds =  100L
+    params = params,
+    data = dataset
   )
   
   return(model)
@@ -89,45 +98,232 @@ lightGBM_function <- function(data, target, ...){
 
 #' LightGBM_predict - predict target for samples in data.
 #'
-#' @param model Trained LightGBM model
-#' @param data Data frame to perform prediction on
+#' @param model Trained LightGBM model.
+#' @param data Data frame to perform prediction on. 
+#' All columns must be <numeric>, <factor> (or <character> if `autofactor` parameter is set to `TRUE`).
+#' @param target Target column to drop for prediction or use in postprocessing (optional).
+#' @param autofactor Logical flag to control whether to encode <character> columns of `data` into <factor>. Default is `FALSE`. 
+#' The encoding is likely to be dependent on order, which is not desirable.
+#' @param postprocess Logical flag to control whether to run postprocessing for the prediction. Default is `FALSE`.
+#' This affects "multiclass" objective
 #'
-#' @return vector od predicted values
+#' @return Vector of predicted values if `model$objective` is "binary", "regression", or "multiclass" and `postprocess` is `FALSE`
+#' @return Matrix of shape `[n_rows, n_classes]` if `model$objective` is "multiclass" and `posprocess` is `TRUE`
 #' @export
 #'
 #' @examples
+#' # More examples present in `lgbm_test_*()` suite of functions
 #' data(iris)
 #' split1 <- sample(c(rep(0, 0.7 * nrow(iris)), rep(1, 0.3 * nrow(iris))))
-#' train <- iris[split1 == 0, ]
-#' model <- lightGBM_function(train, "Species",
-#'                              objective = 'multiclass',
-#'                               num_class = 3)
+#' train <- iris[split1 == 0, ]  
+#' model <- lightGBM_function(
+#'    train,
+#'    "Sepal.Length",
+#'    "regression"
+#' )
 #' test <- iris[split1== 1, ]
-#' test <- test[,1:4]
-#' pred <- lightGBM_predict(model, test)
+#' test <- test[,2:5]
+#' predicted <- lightGBM_predict(model, test) 
 
-LightGBM_predict <- function(model, data){
+
+lightGBM_predict <- function(
+  model, 
+  data, 
+  target=NULL, 
+  autofactor=FALSE, 
+  postprocess=FALSE
+  ){
+  
+  # Drop target if provided
+  if (!is.null(target)) {
+    data_target <- data[, target]
+    data <- data[, !(names(data) %in% target)]
+  }
+  
+  # Convert all <character> columns to <factor> if requested.
+  # The encoding is likely to be dependent on order, which is not desirable.
+  if (autofactor) {
+    data <- lgbm_character_to_factor(data)
+  } else {
+    lgbm_assert_no_characters(data)
+  }
+  
+  # Convert all <factor> columns to <numeric>.
+  categorical_columns <- sapply(data, is.factor)
+  data <- lgbm_factor_to_numeric(data)
+  lgbm_assert_all_numeric(data)
   
   test <- as.matrix(data)
   
-  return(predict(model, test))
+  prediction <- predict(model, test)
+  
+  if (postprocess && model$params$objective == "multiclass") {
+    
+    proba_matrix <- matrix(prediction, nrow = model$params$num_classes)
+    proba_matrix <- t(proba_matrix)
+    rownames(proba_matrix) <- rownames(data)
+    
+    if (is.null(target) || !is.factor(data_target)) {
+      warning(
+        "Postprocessing requested for multiclass objective, ",
+        "but `target` not provided or `data$target` is not a factor. ",
+        "Column names won't be set."
+      )
+    } 
+    else {
+      colnames(proba_matrix) <- levels(data_target)
+    }
+    
+    return(proba_matrix)
+  }
+  
+  return(prediction)
 }
 
 #################################################
 
+#
+# Example usage
+#
 
+#' 
+#' Example regression for `Sepal.Length` from the `iris` dataset
+#' 
+#' @returns Mean squared error for the regression
+#'
+lgbm_test_regression <- function() {
+  set.seed(123)
+  
+  data(iris)
+  
+  split1 <- sample(c(rep(0, 0.7 * nrow(iris)), rep(1, 0.3 * nrow(iris))))
+  train <- iris[split1 == 0, ]  
+  
+  model <- lightGBM_function(
+    train,
+    "Sepal.Length",
+    "regression"
+  )
+  
+  test <- iris[split1== 1, ]
+  test <- test[,2:5]
+  
+  predicted <- lightGBM_predict(model, test) 
+  actual <- iris[split1 == 1, 1]
+  mean((predicted - actual)^2)
+}
 
+#' 
+#' Example binary classification for `Species` from the `iris` dataset
+#' This function builds a model for predicting whether the `Species` is "setosa"
+#' 
+#' @returns Number of misclassified observations
+#'
 
+lgbm_test_binary <- function() {
+  data(iris)
+  
+  setosa <- iris
+  setosa$Is.Setosa = as.numeric(iris$Species == "setosa")
+  setosa$Species <- NULL
+  
+  split1 <- sample(c(rep(0, 0.7 * nrow(setosa)), rep(1, 0.3 * nrow(setosa))))
+  train <- setosa[split1 == 0, ]  
+  
+  model <- lightGBM_function(train, "Is.Setosa", "binary")
+  
+  test <- setosa[split1== 1, ]
+  pred <- lightGBM_predict(model, test, target = "Is.Setosa")
+  
+  sum(round(pred) != test[, "Is.Setosa"])
+}
 
-## Przykład użycia
+#' 
+#' Example multiclass classification for `Species` from the `iris` dataset
+#' This function builds a model for predicting `Species`.
+#' 
+#' @returns Misclassified observation
+#'
 
-data(iris)
+lgbm_test_multiclass <- function() {
+  data(iris)
+  
+  split1 <- sample(c(rep(0, 0.7 * nrow(iris)), rep(1, 0.3 * nrow(iris))))
+  train <- iris[split1 == 0, ]  
+  model <- lightGBM_function(
+    train,
+    "Species",
+    "multiclass"
+  )
+  
+  test <- iris[split1== 1, ]
+  pred <- lightGBM_predict(model, test, target = "Species", postprocess = TRUE)
+  sum(apply(pred, 1, which.max) != as.numeric(test[, "Species"]))
+}
 
-split1 <- sample(c(rep(0, 0.7 * nrow(iris)), rep(1, 0.3 * nrow(iris))))
-train <- iris[split1 == 0, ]  
-model <- lightGBM_function(train, "Species" )
+################################################################################
 
+#
+# Helper functions
+#
 
-test <- iris[split1== 1, ]
-test <- test[,1:4]
-pred <- lightGBM_predict(model, test)
+lgbm_assert_no_characters <- function(df) {
+  if (any(sapply(df, is.character))) {
+    stop(
+      "Character columns present and `autofactor` is set to FALSE. ",
+      "Character columns: ",
+      paste0(colnames(df[,sapply(df, is.character)]), collapse=", ")
+    )
+  }
+}
+# Should fail
+# lgbm_assert_no_characters(
+#   data.frame(x = c("1","2"), y = c(1, 2), z = factor("a", "b"))
+# )
+
+lgbm_assert_all_numeric <- function(df) {
+  if (!all(sapply(df, is.numeric))) {
+    stop(
+      "Non-numeric columns present after pre-processing. Problem columns: ",
+      paste0(colnames(df[,! sapply(df, is.numeric)]), collapse=", ")
+    )
+  }
+}
+# Should fail
+# lgbm_assert_all_numeric(
+#   data.frame(x = c("1","2"), y = c(1, 2), z = factor("a", "b"))
+# )
+
+lgbm_character_to_factor <- function(df) {
+  df[sapply(df, is.character)] <- lapply(
+    df[sapply(df, is.character)],
+    as.factor
+  )
+  df
+}
+
+lgbm_factor_to_numeric <- function(df) {
+  df[sapply(df, is.factor)] <- lapply(
+    df[sapply(df, is.factor)],
+    as.numeric
+  )
+  df
+}
+
+lgbm_df_to_train <- function(df, target, categorical_columns) {
+  target_values <- df[, target]
+  
+  features <- !(names(df) %in% target)
+  data_features <- df[ , features]
+  data_matrix <- as.matrix(data_features[, 1:ncol(data_features)])
+  
+  return(
+    lgb.Dataset(
+      data = data_matrix,
+      label = target_values,
+      free_raw_data = FALSE,
+      categorical_feature = categorical_columns
+    )
+  )
+}
+
